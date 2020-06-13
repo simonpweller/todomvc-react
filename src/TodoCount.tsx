3b07adad780e4ab160fa3b2bd7ef71f75fa46c6b
Simon Weller <s.meierbeck@gmail.com>
2020-06-13 08:49:56 +0200

    destructure props of TodoCount

--- src/TodoCount.tsx
+++ src/TodoCount.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const TodoCount = (props: { count: number }) => (
+const TodoCount = ({ count }: { count: number }) => (
   <span className="todo-count">
-    <strong>{props.count}</strong> {props.count === 1 ? "item" : "items"} left
+    <strong>{count}</strong> {count === 1 ? "item" : "items"} left
   </span>
 );
 
